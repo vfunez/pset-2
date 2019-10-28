63d75522c615c7715a6c5de2481447580c7bb230
Vanya Funez <VFunez@ucvts.tec.nj.us>
2019-10-28 13:23:16 -0400

    eight

---
+++ src/salary.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+let as = (readlineSync.question("Annual salary: "));
+
+const 401K_CONTRIBUTION = 0.07;
+const FEDERAL_INCOME_TAX = 0.157;
+const STATE_INCOME_TAX = 0.0447;
+const SOCAIL_SECURITY_TAX = 0.062;
+const MEDICARE_TAX = 0.0145;
+
+let  = annualsalary / 24
